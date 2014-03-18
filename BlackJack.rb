5298cd0bb6410b5f2302c62f5fad11ee9a51922d
Sanborn <sanbornh@rogers.com>
2014-03-17 20:02:25 -0400

    Batch update

--- BlackJack.rb
+++ BlackJack.rb
@@ -1,13 +1,11 @@
-#  Play BlackJack 1 0n 1 vs the dealer
-# => Test TEST TEST
-#
-#
+# Play black jack 1 0n 1 vs the dealer
+
 
 # ====================================================================
 # ============= Methods are all defined here =========================
 # ====================================================================
 
-#  Query handles all querying for the game.
+#  Query handles most querying for the game (except for depositing)
 #  It asks for user input, then it checks if the user has requested
 #  to perform a valid move. Ex: if the user has asked to hit before
 #  the deck has been dealt then the user is prompted for a new response.
@@ -21,7 +19,7 @@ def query(game_state)
 		if action == "M" 
 			puts "You have $#{$bank_roll}.\n\n" 
 		elsif action == "Q" 
-			abort("Goodbye") 
+			abort "Goodbye" 
 		elsif
 			((action != "H") & (action != "S")) & ((action != "Q") & ((action != "M") & (action != "D")))	
 			puts "(D)eal, (H)it, (S)tand, display (M)oney, or (Q)uit."
@@ -72,14 +70,16 @@ end
 
 #  Evaluates the hands and returns a new game state 
 #  Ex: push, bust, win, lose
-def evaluate(game_state, player_hand, player_total, dealers_hand, dealers_actual_total, player_turn)
+def evaluate(game_state, player_hand, dealers_hand, player_turn)
+	player_total = get_total(player_hand)
+	dealers_total = get_total(dealers_hand)
 
 	if (game_state == "cards_dealt") && (player_turn == true)
-		if (player_total == 21) && (dealers_actual_total == 21)						# If dealer and player are both dealt Black Jack then push
+		if (player_total == 21) && (dealers_total == 21)						# If dealer and player are both dealt Black Jack then push
 			return "push"
-		elsif (dealers_actual_total == 21) && (dealers_hand.length == 2) 	# If dealer is dealt Black Jack but player is not, then dealer wins
+		elsif (dealers_total == 21) && (dealers_hand.length == 2) 	# If dealer is dealt Black Jack but player is not, then dealer wins
 			return "dealer blackJack"
-		elsif (player_total == 21) && (player_hand.length == 2)						# If player is dealt Black Jack but dealer is not, then player wins
+		elsif (player_total == 21) && (player_hand.length == 2)			# If player is dealt Black Jack but dealer is not, then player wins
 			return "player blackJack"
 		elsif (player_total > 21)
 			return "bust"
@@ -87,11 +87,11 @@ def evaluate(game_state, player_hand, player_total, dealers_hand, dealers_actual
 			return "nothing"
 		end
 	elsif player_turn == false
-		if dealers_actual_total > 21
+		if dealers_total > 21
 			return "bust"
-		elsif player_total > dealers_actual_total
+		elsif player_total > dealers_total
 			return "player win"
-		elsif dealers_actual_total > player_total
+		elsif dealers_total > player_total
 			return "dealer win"
 		else
 			return "push"
@@ -100,7 +100,7 @@ def evaluate(game_state, player_hand, player_total, dealers_hand, dealers_actual
 end
 
 # Displays the players cards and Dealers cards with their associated values
-def display(player_turn, players_hand, dealers_hand, player_total, dealer_visible_total, dealers_actual_total)
+def display(player_turn, players_hand, dealers_hand)
 	lineWidth = 25
 
 	if players_hand.length > dealers_hand.length			# Take the hand that has more cards and store the number 
@@ -150,11 +150,11 @@ def display(player_turn, players_hand, dealers_hand, player_total, dealer_visibl
 	# Value of Players hand and visible dealer card
 	print "Total:".ljust(10)
 	if player_turn 
-		print " #{dealer_visible_total}".ljust(lineWidth + 1)
-		puts "#{player_total}\n\n".ljust(lineWidth + 1)
+		print " #{$deck[dealers_hand[0]]}".ljust(lineWidth + 1)
+		puts "#{get_total(players_hand)}\n\n".ljust(lineWidth + 1)
 	else
-		print " #{dealers_actual_total}".ljust(lineWidth + 1)
-		puts "#{player_total}\n\n".ljust(lineWidth + 1)
+		print " #{get_total(dealers_hand)}".ljust(lineWidth + 1)
+		puts "#{get_total(players_hand)}\n\n".ljust(lineWidth + 1)
 	end
 end
 
@@ -181,15 +181,38 @@ def blackJack(who_won)
 	end
 end
 
+# Returns the total value of a hand
+def get_total(hand)
+	total = 0
+	hand.each { |i| total = total + $deck[i] }
+	return total
+end
+
+# Query user to deposit money and accept input of money
+def deposit
+	while true
+		print "How much money would you like to deposit? "
+		response = gets.chomp
+		if response.upcase == "Q"
+			abort("Goodbye") 
+		elsif response.to_i >= 10
+			$bank_roll = response.to_i
+			break
+		end
+	end
+	puts "Okay, you have $#{$bank_roll}.\n"
+end
+
+
 
 
 #  Deck is a hash storing all the values of a single deck
 #  It is used to get the values of the cards that are dealt
 #  At the beginning of the game the deck is full.
 #  **NOTE: Aces have a starting value of 11 and are assumed to be 11
-#  unless player with ace busts, in which case the value of the ace is 
-#  switched to 1.
-deck = {
+#  unless player with ace busts. If that does happen, then the ace in
+#  their hand is assigned to the alternative Ace value (Aalt)
+$deck = {
 	"AH" => 11, "2H" => 2, "3H" => 3, "4H" => 4, "5H" => 5, "6H" => 6, "7H" => 7, "8H" => 8, "9H" => 9, "10H" => 10, "JH" => 10, "QH" => 10, "KH" => 10,
 	"AD" => 11, "2D" => 2, "3D" => 3, "4D" => 4, "5D" => 5, "6D" => 6, "7D" => 7, "8D" => 8, "9D" => 9, "10D" => 10, "JD" => 10, "QD" => 10, "KD" => 10,
 	"AC" => 11, "2C" => 2, "3C" => 3, "4C" => 4, "5C" => 5, "6C" => 6, "7C" => 7, "8C" => 8, "9C" => 9, "10C" => 10, "JC" => 10, "QC" => 10, "KC" => 10,
@@ -197,75 +220,76 @@ deck = {
 }
 
 # Starting bank roll is set to $100
-$bank_roll = 100
+$bank_roll = 0
 
 # ======================================================
 # ========= Player interaction begins here =============
 # ======================================================
+#binding.pry
 
 puts "Welcome to BlackJack 1.0"
-puts "You have $100. Bets are $10."
+puts "Bets are $10."
+deposit													# Get a deposit from the user
+
 
 while true
 	#  Builds an array of keys from the deck without associated values
 	#  This deck is rebuilt each time the player chooses to deal new cards
 	#  so that dealing always happens from a full deck.
-	$deck_keys = deck.keys 
+	$deck_keys = $deck.keys 
 
 	# Set the beginning state of the game
-	possible_states = ["cards_not_dealt", "cards_dealt", "bust", "after_dealer_move"]
 	game_state = "cards_not_dealt"
 	players_turn = true
 	query_response =
 
+	# If the bankroll has gone below the minimum bet amount then the player
+	# must deposit more money to keep playing
+	if $bank_roll < 10
+		puts "You do not have enough money to continue playing."
+		deposit
+	end
+
 	while query_response != "Q"
-		query_response = query(game_state)
-		#puts "1"
+		query_response = query(game_state)				# Gets input from the user
 
 		if query_response == "D"									# If the user chose to deal, deal the cards
-			#puts "2"
 			starting_hands = deal										# Pass the starting hands out 
+			players_hand = starting_hands[0]				# Assign the player his/her hand
+			dealers_hand = starting_hands[1]				# Ditto above but with dealer's hand
+
 			game_state = "cards_dealt"							# The deck has now been dealt so the game state changes
-		
-			players_hand = starting_hands[0]																					# the players hand is stored into an array
-			player_total = deck[players_hand[0]] + deck[players_hand[1]]							# the value of the players hand
+		  display(players_turn, players_hand, dealers_hand)
 
-			dealers_hand = starting_hands[1]																					# ditto above but with dealers hand
-			dealer_visible_total = deck[dealers_hand[0]]                    					# set the visible total of the dealers hand 
-			dealers_actual_total = deck[dealers_hand[0]] + deck[dealers_hand[1]]			# set the dealers actual hand value
-
-		  display(players_turn, players_hand, dealers_hand, player_total, dealer_visible_total, dealers_actual_total)
-
-		  if evaluate(game_state, players_hand, player_total, dealers_hand, dealers_actual_total, players_turn) == ("player blackJack" || "dealer blackJack")
-				blackJack(evaluate(game_state, players_hand, player_total, dealers_hand, dealers_actual_total, players_turn))
+		  if evaluate(game_state, players_hand, dealers_hand, players_turn) == ("player blackJack" || "dealer blackJack")
+				blackJack(evaluate(game_state, players_hand, dealers_hand, players_turn))
 				game_state = "cards_not_dealt"
 			end
-		elsif query_response == "H"																						# If player hits then deal a new card
-			#puts "3"
-			players_hand << dealNewCard																					# Players hand is updated to include the new card
-			player_total += deck[players_hand[players_hand.length - 1]]					# The new card value is added to the value of the rest of the players hand	
+		elsif query_response == "H"								# If player hits then deal a new card
+			players_hand << dealNewCard							# Players hand is updated to include the new card
 			
-			display(players_turn, players_hand, dealers_hand, player_total, dealer_visible_total, dealers_actual_total)
+			display(players_turn, players_hand, dealers_hand)
 			
-			if evaluate(game_state, players_hand, player_total, dealers_hand, dealers_actual_total, players_turn) == "bust"														# If the player busts then change the bank roll and display bust message
+			# If the player busts then change the bank roll and display bust message
+			if evaluate(game_state, players_hand, dealers_hand, players_turn) == "bust"				
 				bust
-				break																																																										  # Break will return game to its starting state at which point everything proceeds from beginning
-			elsif evaluate(game_state, players_hand, player_total, dealers_hand, dealers_actual_total, players_turn) == "push"												# If push right off the bat then return game to starting state
+				break		# Break will return game to its starting state at which point everything proceeds from beginning
+
+			# If push right off the bat then return game to starting state																																				
+			elsif evaluate(game_state, players_hand, dealers_hand, players_turn) == "push"		
 				push
 				break
 			end
 
 		elsif query_response == "S"
-			#puts "4"
-			#puts "#{game_state}"
 			players_turn = false
-			display(players_turn, players_hand, dealers_hand, player_total, dealer_visible_total, dealers_actual_total)
-			while dealers_actual_total <= 16
+			display(players_turn, players_hand, dealers_hand)
+
+			while get_total(dealers_hand) <= 16
 				dealers_hand << dealNewCard
-				dealers_actual_total += deck[dealers_hand[dealers_hand.length - 1]]
-				display(players_turn, players_hand, dealers_hand, player_total, dealer_visible_total, dealers_actual_total)
+				display(players_turn, players_hand, dealers_hand)
 			end
-			result = evaluate(game_state, players_hand, player_total, dealers_hand, dealers_actual_total, players_turn)
+			result = evaluate(game_state, players_hand, dealers_hand, players_turn)
 			if result == "push"
 				push
 				break
@@ -284,7 +308,6 @@ while true
 				$bank_roll += 10
 				break
 			end
-			#display(players_turn, players_hand, dealers_hand, player_total, dealer_visible_total, dealers_actual_total)
 		end
 	end
 end	
---
+++ PersonTest.rb
@@ -0,0 +1,49 @@
+class Person
+	@@num_people = 0
+	def initialize(name)
+		@name = name
+		@@num_people += 1
+	end
+
+	def display_name
+		puts "Hi my name is #{@name}."
+	end
+
+	def self.display_num_people
+		puts "There are #{@@num_people} people."
+	end
+
+	def self.kill(person)
+		person = nil
+		@@num_people -= 1
+	end
+
+	# def finalize
+	# 	@@num_people -= 1
+	# end
+end
+
+person1 = Person.new("sanborn")
+person2 = Person.new("Raz")
+
+#person1.display_num_people
+
+Person.display_num_people
+
+person1.display_name
+person2.display_name
+
+@name = "Penelope"
+#puts person1.@name
+puts @name
+
+Person.kill(person1)
+
+Person.display_num_people
+
+binding.pry
+
+
+
+
+
